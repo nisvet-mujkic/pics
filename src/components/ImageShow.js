cb22f982408e09264dd7c4c64d9ee39f8a0a4ede
Nisvet Mujkic <nisvet.mujkic@outlook.com>
2023-03-10 17:00:54 +0100

    styling

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,5 +1,11 @@
-function ImageShow() {
-  return <div>ImageShow</div>;
+import "./ImageShow.css";
+
+function ImageShow({ image }) {
+  return (
+    <div>
+      <img src={image.urls.small} alt={image.alt_description} />
+    </div>
+  );
 }
 
 export default ImageShow;
